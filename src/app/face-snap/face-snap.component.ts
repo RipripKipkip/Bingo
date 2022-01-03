96f2d8fc1a85adcd3e3cccc1f0b7d25bb1e176bd
Ingrid <kipkipdesigns@gmail.com>
2022-01-04 00:34:52 +0100

    Basic

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent implements OnInit {
+  snaps!: number;
+
+  ngOnInit(): void {
+    this.snaps = 6;
+  }
+
+}
